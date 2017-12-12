7a9a849ccbe1df9dfdfce5419512da7f67355ea7
Livio Brunner <livio.brunner.lb1@gmail.com>
2017-12-12 16:05:36 +0100

    Add ts typings

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for pokemongo-game-master 1.0.0
+// Project: https://github.com/BrunnerLivio/pokemongo-game-master
+// Definitions by: Livio Brunner <https://github.com/BrunnerLivio>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/**
+ * Collection of the decoded GAME_MASTER-protobuf files
+ */
+export class PokemongoGameMaster {
+    static getVersion(version: string, format: "protobuf" | "json"): Promise<string|object>;
+}
