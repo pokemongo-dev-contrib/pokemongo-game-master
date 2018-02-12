579e08135147ec280c3698f5836ebfc3bb4f57b8
Livio Brunner <livio.brunner.lb1@gmail.com>
2018-02-12 17:03:47 +0100

    Update bash script

--- bin/add-version.sh
+++ bin/add-version.sh
@@ -69,9 +69,13 @@ NEW_JSON_PATH="${NEW_VERSION_FOLDER}/GAME_MASTER.json"
 
 echo $NEW_PROTOBUF_PATH
 
-mkdir $NEW_VERSION_FOLDER
+# Add folder only if needed
+mkdir -p $NEW_VERSION_FOLDER
 
-cp $PROTOBUF_FILE $NEW_PROTOBUF_PATH
+# Check if file is already in the destination
+if [ "$(stat -c "%d:%i" $PROTOBUF_FILE)" != "$(stat -c "%d:%i" $NEW_PROTOBUF_PATH)" ]; then
+  cp $PROTOBUF_FILE $NEW_PROTOBUF_PATH
+fi
 
 docker run -it --rm --name my-maven-project -v "$HOME/.m2":/root/.m2 -v `pwd`:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn clean package exec:java -Dexec.mainClass="com.pokebattler.gamemaster.GenerateJSON" -Dexec.args="${PROTOBUF_FILE} versions/${VERSION_TO_ADD}/GAME_MASTER.json"
 
