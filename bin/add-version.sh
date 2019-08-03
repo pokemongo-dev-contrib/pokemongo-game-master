5acd1a4e0e4b22fe0cf9f7d17a642024ebe6f72b
--=FurtiF™=-- <msntchat@hotmail.fr>
2019-08-03 02:27:57 +0200

    format file

--- bin/add-version.sh
+++ bin/add-version.sh
@@ -11,12 +11,11 @@ VERSIONS_FOLDER="${ROOTFOLDER}/versions"
 # Returns:
 #   None
 #######################################
-print_error_and_exit()
-{
-    UNEXPECTED_TRAP_MESSAGE=""
+print_error_and_exit() {
+  UNEXPECTED_TRAP_MESSAGE=""
 
-    echo -e "\nError: $1"
-    exit 1
+  echo -e "\nError: $1"
+  exit 1
 }
 
 #######################################
@@ -26,9 +25,8 @@ print_error_and_exit()
 # Returns:
 #   None
 #######################################
-print_usage()
-{
-    cat <<EOF
+print_usage() {
+  cat <<EOF
 pokemongo-game-master add version
 
 Required arguments:
@@ -41,39 +39,35 @@ Sample usage:
 ./add-version.sh -f ./GAME_MASTER.protobuf -v "0.83.2-1" --latest
 
 EOF
-    return 0;
+  return 0
 }
 
-while true
-do
-    case "$1" in
-        -f|--file)           PROTOBUF_FILE=$2 && shift 2;;
-        -h|--help)           print_usage && exit 1;;
-        -t|--timestamp)      TIMESTAMP=$2 && shift 2;;
-        --latest)            LATEST=true && shift 1;;
-        *)                   break;;
-    esac
+while true; do
+  case "$1" in
+  -f | --file) PROTOBUF_FILE=$2 && shift 2 ;;
+  -h | --help) print_usage && exit 1 ;;
+  -t | --timestamp) TIMESTAMP=$2 && shift 2 ;;
+  --latest) LATEST=true && shift 1 ;;
+  *) break ;;
+  esac
 done
 
 if [ "${PROTOBUF_FILE}" == "" ]; then
-    print_error_and_exit "Missing protobuf file"
+  print_error_and_exit "Missing protobuf file"
 fi
-
-
 
 # Generate JSON
 mvn clean package exec:java -Dexec.mainClass="com.pokebattler.gamemaster.GenerateJSON" -Dexec.args="${PROTOBUF_FILE} GAME_MASTER.json"
 
 if [ "${TIMESTAMP}" == "" ]; then
-    # Get timestampMs from new JSON file
-    json_content=$(cat GAME_MASTER.json)
-    TIMESTAMP=$(echo $json_content | grep -oP '"timestampMs": "([0-9]+)"'  | grep -oP "[0-9]+")
+  # Get timestampMs from new JSON file
+  json_content=$(cat GAME_MASTER.json)
+  TIMESTAMP=$(echo $json_content | grep -oP '"timestampMs": "([0-9]+)"' | grep -oP "[0-9]+")
 fi
 
 NEW_VERSION_FOLDER="${VERSIONS_FOLDER}/${TIMESTAMP}"
 NEW_PROTOBUF_PATH="${NEW_VERSION_FOLDER}/GAME_MASTER.protobuf"
 NEW_JSON_PATH="${NEW_VERSION_FOLDER}/GAME_MASTER.json"
-
 
 # Add folder only if needed
 mkdir -p $NEW_VERSION_FOLDER
@@ -83,17 +77,16 @@ cp -rf GAME_MASTER.json $NEW_JSON_PATH
 
 git add "$NEW_PROTOBUF_PATH" "$NEW_JSON_PATH"
 
-if [ "$LATEST" == true ] ; then
-    # Copy into latest folder
-    cp -Tr "$NEW_VERSION_FOLDER/" "$VERSIONS_FOLDER/latest"
-    # Update latest version.txt
-    rm -f versions/latest-version.txt
-    echo $TIMESTAMP >> versions/latest-version.txt
-    git add "$VERSIONS_FOLDER/latest/GAME_MASTER.protobuf" "$VERSIONS_FOLDER/latest/GAME_MASTER.json" "$VERSIONS_FOLDER/latest-version.txt"
+if [ "$LATEST" == true ]; then
+  # Copy into latest folder
+  cp -Tr "$NEW_VERSION_FOLDER/" "$VERSIONS_FOLDER/latest"
+  # Update latest version.txt
+  rm -f versions/latest-version.txt
+  echo $TIMESTAMP >>versions/latest-version.txt
+  git add "$VERSIONS_FOLDER/latest/GAME_MASTER.protobuf" "$VERSIONS_FOLDER/latest/GAME_MASTER.json" "$VERSIONS_FOLDER/latest-version.txt"
 fi
 
 git commit -m "Update GAME_MASTER with timestamp ${TIMESTAMP}"
 
 echo "DONE"
 echo "Run 'git push origin master --tags' to push the current changes"
-
