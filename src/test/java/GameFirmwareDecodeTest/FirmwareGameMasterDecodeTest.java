896f8e9017a8fd493e8a088847cfe34c55b2be78
Furtif <msntchat@hotmail.fr>
2020-02-03 00:49:11 +0100

    update to good stuff

--- src/test/java/GameFirmwareDecodeTest/FirmwareGameMasterDecodeTest.java
+++ src/test/java/GameFirmwareDecodeTest/FirmwareGameMasterDecodeTest.java
@@ -1,6 +1,6 @@
 package GameFirmwareDecodeTest;
 
-import POGOProtos.Networking.Responses.*;
+import POGOProtos.Networking.Game.GameGmTemplates.Responses.*;
 import com.google.protobuf.util.*;
 import org.junit.*;
 
@@ -11,7 +11,7 @@ public class FirmwareGameMasterDecodeTest {
 	public void TestFirmwareGameMasterDecode() throws Exception {
 		try (InputStream is = getClass().getResourceAsStream("/FIRMWARE_GAME_MASTER.protobuf"))
         {
-			DownloadItemTemplatesResponse response = DownloadItemTemplatesResponse.parseFrom(is);
+			DownloadGameMasterTemplatesResponse response = DownloadGameMasterTemplatesResponse.parseFrom(is);
 			JsonFormat.Printer printer = JsonFormat.printer();
 			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/FIRMWARE_GAME_MASTER.json")))
             {
