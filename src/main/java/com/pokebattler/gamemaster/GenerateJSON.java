daa6a71dcd9dde025fe60a6ab967b5787342a68f
Furtif <msntchat@hotmail.fr>
2020-03-21 19:35:05 +0100

    mode dec in c#

--- src/main/java/com/pokebattler/gamemaster/GenerateJSON.java
+++ src/main/java/com/pokebattler/gamemaster/GenerateJSON.java
@@ -9,11 +9,28 @@ public class GenerateJSON {
 	public GenerateJSON() {
 	}
 
-	public void writeJSON(InputStream is, OutputStream os) throws IOException {
-		//DownloadItemTemplatesResponse response = DownloadItemTemplatesResponse.parseFrom(is);
-		DownloadGmTemplatesResponse response = DownloadGmTemplatesResponse.parseFrom(is);
-		JsonFormat.Printer printer = JsonFormat.printer();
-		try (OutputStreamWriter writer = new OutputStreamWriter(os)) {
+	public void writeJSON(java.io.InputStream is, java.io.OutputStream os) throws java.io.IOException {
+		POGOProtos.Networking.Responses.DownloadGmTemplatesResponse response = POGOProtos.Networking.Responses.DownloadGmTemplatesResponse.parseFrom(is);
+
+		/*
+		//C# code mode needs fix it for java
+		com.pokebattler.gamemaster.GameMaster master = new com.pokebattler.gamemaster.GameMaster();
+		master.Result = response.Result;
+		master.Templates = new RepeatedField<GameMasterClientTemplate>();
+		master.DeletedTemplates = response.DeletedTemplate;
+		master.BatchId = response.BatchId;
+		master.PageOffset = response.PageOffset;
+		master.ExperimentId = response.ExperimentId;
+
+		for (var i : response.Template)
+		{
+			master.Templates.Add(GameMasterClientTemplate.parseFrom(i.Data));
+		}
+        */
+
+		com.google.protobuf.util.JsonFormat.Printer printer = com.google.protobuf.util.JsonFormat.printer();
+		try (java.io.OutputStreamWriter writer = new java.io.OutputStreamWriter(os)) {
+			//printer.appendTo(master, writer);
 			printer.appendTo(response, writer);
 		}
 	}
@@ -36,4 +53,14 @@ public class GenerateJSON {
 			gen.writeJSON(is, os);
 		}
 	}
+}
+
+final class GameMaster
+{
+	public DownloadGmTemplatesResponse.Result Result;
+	public java.util.List<POGOProtos.Networking.Responses.DownloadItemTemplatesResponse.GameMasterClientTemplate> Templates;
+	public java.util.List<String> DeletedTemplates;
+	public long BatchId;
+	public int PageOffset;
+	public java.util.List<Integer> ExperimentId;
 }
