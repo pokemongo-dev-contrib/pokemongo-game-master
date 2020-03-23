a86876364dc74f0833c98e1670ce745510b7187d
Furtif <msntchat@hotmail.fr>
2020-03-23 16:46:48 +0100

    V2 gm fine

--- src/main/java/com/pokebattler/gamemaster/GenerateJSON.java
+++ src/main/java/com/pokebattler/gamemaster/GenerateJSON.java
@@ -1,46 +1,19 @@
 package com.pokebattler.gamemaster;
 
-import POGOProtos.Networking.Responses.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import POGOProtos.Data.*;
+import com.google.protobuf.util.*;
 
 import java.io.*;
-import java.util.*;
 
 public class GenerateJSON {
 	public GenerateJSON() {
 	}
 
 	public void writeJSON(InputStream is, OutputStream os) throws IOException {
-		DownloadGmTemplatesResponse response = DownloadGmTemplatesResponse.parseFrom(is);
-		GameMaster master = new GameMaster();
-		//master.Result = response.getResult();
-		master.Templates = new ArrayList();
-		master.DeletedTemplates = response.getDeletedTemplateList();
-		master.BatchId = response.getBatchId();
-		//master.PageOffset = response.getPageOffset();
-		master.ExperimentId = response.getExperimentIdList();
-
-		for (DownloadGmTemplatesResponse.ClientGameMasterTemplate i : response.getTemplateList())
-		{
-			master.Templates.add(DownloadItemTemplatesResponse.GameMasterClientTemplate.parseFrom(i.getData()));
-		}
-
-		//TODO: this is modified response and get error try to fix this
-		/*
+		GameMasterDecoder response = GameMasterDecoder.parseFrom(is);
 		JsonFormat.Printer printer = JsonFormat.printer();
 		try (OutputStreamWriter writer = new OutputStreamWriter(os)) {
-			printer.appendTo(master, writer);
-		}
-		*/
-
-		// pretty print
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		// Java objects to File
-		try (OutputStreamWriter writer = new OutputStreamWriter(os)) {
-			gson.toJson(master, writer);
-		} catch (IOException e) {
-			e.printStackTrace();
+			printer.appendTo(response, writer);
 		}
 	}
 
@@ -62,13 +35,4 @@ public class GenerateJSON {
 			gen.writeJSON(is, os);
 		}
 	}
-}
-
-final class GameMaster {
-	//public DownloadGmTemplatesResponse.Result Result;
-	public List<DownloadItemTemplatesResponse.GameMasterClientTemplate> Templates;
-	public List<String> DeletedTemplates;
-	public long BatchId;
-	//public int PageOffset;
-	public List<Integer> ExperimentId;
 }
--- src/test/java/GameFirmwareDecodeTest/FirmwareGameMasterDecodeTest.java
+++ src/test/java/GameFirmwareDecodeTest/FirmwareGameMasterDecodeTest.java
@@ -1,54 +1,20 @@
 package GameFirmwareDecodeTest;
 
-import POGOProtos.Networking.Responses.*;
-import com.google.gson.*;
+import POGOProtos.Data.*;
+import com.google.protobuf.util.*;
 import org.junit.*;
 
 import java.io.*;
-import java.util.*;
 
 public class FirmwareGameMasterDecodeTest {
 	@Test
 	public void TestFirmwareGameMasterDecode() throws Exception {
 		try (InputStream is = getClass().getResourceAsStream("/FIRMWARE_GAME_MASTER.protobuf")) {
-			DownloadGmTemplatesResponse response = DownloadGmTemplatesResponse.parseFrom(is);
-			GameMaster master = new GameMaster();
-			//master.Result = response.getResult();
-			master.Templates = new ArrayList();
-			master.DeletedTemplates = response.getDeletedTemplateList();
-			master.BatchId = response.getBatchId();
-			//master.PageOffset = response.getPageOffset();
-			master.ExperimentId = response.getExperimentIdList();
-
-			for (DownloadGmTemplatesResponse.ClientGameMasterTemplate i : response.getTemplateList()) {
-				master.Templates.add(DownloadItemTemplatesResponse.GameMasterClientTemplate.parseFrom(i.getData()));
-			}
-
-			//TODO: this is modified response and get error try to fix this
-		    /*
+			GameMasterDecoder response = GameMasterDecoder.parseFrom(is);
 		    JsonFormat.Printer printer = JsonFormat.printer();
-		    try (OutputStreamWriter writer = new OutputStreamWriter(os)) {
-			     printer.appendTo(master, writer);
+		    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/FIRMWARE_GAME_MASTER.json"))) {
+			     printer.appendTo(response, writer);
 		    }
-		    */
-
-			// pretty print
-			Gson gson = new GsonBuilder().setPrettyPrinting().create();
-			// Java objects to File
-			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/FIRMWARE_GAME_MASTER.json"))) {
-				gson.toJson(master, writer);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
-}
-
-final class GameMaster {
-	//public DownloadGmTemplatesResponse.Result Result;
-	public List<POGOProtos.Networking.Responses.DownloadItemTemplatesResponse.GameMasterClientTemplate> Templates;
-	public List<String> DeletedTemplates;
-	public long BatchId;
-	//public int PageOffset;
-	public List<Integer> ExperimentId;
 }
