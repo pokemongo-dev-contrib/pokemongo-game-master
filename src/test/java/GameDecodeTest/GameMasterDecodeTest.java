ac16aa64414301bbbccd3cc92127141f4ce4a9aa
Furtif <msntchat@hotmail.fr>
2020-04-24 12:29:20 +0200

    uses latest folders

--- src/test/java/GameDecodeTest/GameMasterDecodeTest.java
+++ src/test/java/GameDecodeTest/GameMasterDecodeTest.java
@@ -1,4 +1,4 @@
-package GameFirmwareDecodeTest;
+package GameDecodeTest;
 
 import POGOProtos.Data.*;
 import com.google.protobuf.util.*;
@@ -6,13 +6,13 @@ import org.junit.*;
 
 import java.io.*;
 
-public class FirmwareGameMasterDecodeTest {
+public class GameMasterDecodeTest {
 	@Test
 	public void TestFirmwareGameMasterDecode() throws Exception {
-		try (InputStream is = getClass().getResourceAsStream("/FIRMWARE_GAME_MASTER.protobuf")) {
+		try (InputStream is = getClass().getResourceAsStream("versions/latest/V2_GAME_MASTER.protobuf")) {
 			GameMasterDecoder response = GameMasterDecoder.parseFrom(is);
 			JsonFormat.Printer printer = JsonFormat.printer();
-			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/FIRMWARE_GAME_MASTER.json"))) {
+			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/V2_GAME_MASTER.json"))) {
 				printer.appendTo(response, writer);
 			}
 		}
