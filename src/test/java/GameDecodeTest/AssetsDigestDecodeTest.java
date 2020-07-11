fe1f6110fb63cd67da2a02067077cc7b47c6c519
Furtif <msntchat@hotmail.fr>
2020-07-12 01:11:47 +0200

    sync for other mode

--- src/test/java/GameDecodeTest/AssetsDigestDecodeTest.java
+++ src/test/java/GameDecodeTest/AssetsDigestDecodeTest.java
@@ -9,7 +9,7 @@ import java.io.*;
 public class AssetsDigestDecodeTest {
 	@Test
 	public void TestAssetsDigestDecode() throws Exception {
-		try (InputStream is = getClass().getResourceAsStream("assetdigest/versions/latest/ASSET_DIGEST")) {
+		try (InputStream is = getClass().getResourceAsStream("assetdigest/versions/latest/ASSET_DIGEST.protobuf")) {
 			AssetDigestOutProto response = AssetDigestOutProto.parseFrom(is);
 			JsonFormat.Printer printer = JsonFormat.printer();
 			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/ASSET_DIGEST.json"))) {
--- src/test/java/GameDecodeTest/GameMasterDecodeTest.java
+++ src/test/java/GameDecodeTest/GameMasterDecodeTest.java
@@ -9,7 +9,7 @@ import java.io.*;
 public class GameMasterDecodeTest {
 	@Test
 	public void TestFirmwareGameMasterDecode() throws Exception {
-		try (InputStream is = getClass().getResourceAsStream("versions/latest/v2_GAME_MASTER")) {
+		try (InputStream is = getClass().getResourceAsStream("versions/latest/v2_GAME_MASTER.protobuf")) {
 			PlatformDownloadGmTemplatesResponseProto response = PlatformDownloadGmTemplatesResponseProto.parseFrom(is);
 			JsonFormat.Printer printer = JsonFormat.printer();
 			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/v2_GAME_MASTER.json"))) {
