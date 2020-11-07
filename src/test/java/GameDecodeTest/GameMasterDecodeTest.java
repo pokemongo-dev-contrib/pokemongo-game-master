36c706b0f63f67de8e65dc3c2a832ad018fd139e
Furtif <msntchat@hotmail.fr>
2020-11-08 00:30:10 +0100

    1604721081869

--- src/test/java/GameDecodeTest/GameMasterDecodeTest.java
+++ src/test/java/GameDecodeTest/GameMasterDecodeTest.java
@@ -10,7 +10,7 @@ public class GameMasterDecodeTest {
 	@Test
 	public void TestFirmwareGameMasterDecode() throws Exception {
 		try (InputStream is = getClass().getResourceAsStream("versions/latest/v2_GAME_MASTER.protobuf")) {
-			DownloadGmTemplatesResponseProto response = DownloadGmTemplatesResponseProto.parseFrom(is);
+			PlatformDownloadGmTemplatesResponseProto response = PlatformDownloadGmTemplatesResponseProto.parseFrom(is);
 			JsonFormat.Printer printer = JsonFormat.printer();
 			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("target/test-classes/v2_GAME_MASTER.json"))) {
 				printer.appendTo(response, writer);
